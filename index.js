3604f2ffd42aa09ba7b2d63cd4878de5f57be91b
Cat Whitmer <catwhitmer@gmail.com>
2021-05-27 16:03:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ main.remove();
 let newHeader = document.createElement('h1');
 
 document.body.appendChild(newHeader);
+
+let element = document.querySelector('h1');
+element.innerHTML = 'is the champion';
