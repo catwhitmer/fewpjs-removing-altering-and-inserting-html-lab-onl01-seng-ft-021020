350b3ece365213d62c29393e5612be80160646bb
Cat Whitmer <catwhitmer@gmail.com>
2021-05-27 15:57:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,3 @@ main.remove();
 
 let newHeader = document.createElement('h1');
 
-for (let i = 0; i < 3; i++) {
-  let li = document.createElement('')
-  li.innerHTML = (i + 1).toString()
-  h1.appendChild(li)
-}
- 
-element.appendChild(ul)
